02424f9a5e669b0aa300afc89123c6bf05c469f2
voynpower <hh4868047@gmail.com>
2024-11-15 01:32:04 +0000

    test

--- demo/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ demo/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,10 +2,20 @@ package com.example.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class DemoController{
+	@GetMapping("/hello")
+	fun hello() : String {
+		return "Hello render"
+	}
 }
